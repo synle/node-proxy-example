e8f14c2657a67a76500662857df8ef3d4d6a223b
Sy Le <lenguyensy@gmail.com>
2024-01-04 08:37:16 -0800

    Update start-https-server.js

--- start-https-server.js
+++ start-https-server.js
@@ -1,41 +1,39 @@
-var https = require('https');
-var httpProxy = require('http-proxy');
-var fs = require('fs');
+var port = "9001";
+var proxyUrl = "http://127.0.0.1:8000";
+var keyPath = "./cert/key.pem";
+var certPath = "./cert/cert.pem";
 
+var https = require("https");
+var httpProxy = require("http-proxy");
+var fs = require("fs");
 
-var port = '9001';
-var proxyUrl = 'http://127.0.0.1:8000';
-
-var proxyOptions = {
-    target: proxyUrl
-};
-
-var proxy = httpProxy.createProxyServer(proxyOptions); // See (†)
-proxy.on('error', function(e) {
-    console.log('PROXY ERROR: ', e);
+var proxy = httpProxy.createProxyServer({
+  target: proxyUrl,
 });
 
-var serverOptions = {
-    key: fs.readFileSync('./cert/key.pem'),
-    cert: fs.readFileSync('./cert/cert.pem')
-};
-
+proxy.on("error", function (e) {
+  console.log("PROXY ERROR: ", e);
+});
 
 // server itself
-var server = https.createServer(serverOptions, function(req, res) {
-        var requestUrl = req.url;
+var server = https
+  .createServer(
+    {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    },
+    function (req, res) {
+      var requestUrl = req.url;
 
-        if(requestUrl.indexOf('/api') >= 0){
-            proxy.web(req, res, proxyOptions);
-        } else {
-            res.writeHead(200, {"Content-Type": "text/plain"});
-            res.end("Responded from the Web Itself : Hello World\n");
-        }
-    })
-    .listen(port);
+      if (requestUrl.indexOf("/api") >= 0) {
+        proxy.web(req, res, proxyOptions);
+      } else {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Responded from the Web Itself : Hello World\n");
+      }
+    },
+  )
+  .listen(port);
 
-
-console.log('proxy', proxyUrl);
-console.log('listen to port', port);
-
-
+console.log("proxy", proxyUrl);
+console.log("listen to port", port);
